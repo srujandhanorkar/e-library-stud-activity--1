391a10a90ff546ad8ff1126dfda6bb09629a362f
WHJR_PRO-1:4 <72431927+rashmi-sathya@users.noreply.github.com>
2022-01-13 18:54:43 +0530

    Add files via upload

---
+++ components/BottomTabNavigator.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+//import the libraries
+
+
+import TransactionScreen from "../screens/Transaction";
+import SearchScreen from "../screens/Search";
+// create tab 
+
+export default class BottomTabNavigator extends Component {
+  render() {
+    return (
+      //add the code for add navigationcontainer
+
+      
+    );
+  }
+}
