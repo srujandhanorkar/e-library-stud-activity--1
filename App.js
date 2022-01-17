241aef13757356b98cb3f023b6e5c7c393931d39
WHJR_PRO-1:4 <72431927+rashmi-sathya@users.noreply.github.com>
2022-01-17 15:05:49 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import BottomTabNavigator from "./components/BottomTabNavigator";
 
 export default class App extends Component {
   render() {
-    //add the return satement for BottomTabNavigator
-    return <BottomTabNavigator />;
+    //add the return satement for BottomTabNavigator- challenge 4
   }
 }
